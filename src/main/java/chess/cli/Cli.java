bdfb3e7e83966a5790b77c673edcbe4f0aa0201b
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2021-04-28 18:31:06 +0200

    Input moves on Console

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -4,17 +4,29 @@ package chess.cli;
 import chess.model.Board;
 import chess.model.Move;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Cli {
     protected static Board board = new Board();
 
     public static void main(String[] args) {
+
         while (true) {
-            System.out.println(board.showBoard());
-            String input = "e2-e4";
-            Move move = new Move(input);
-            board.applyMove(move);
-            System.out.println(board.showBoard());
-            break; //todo playerInput
+            String output = board.showBoard();
+            System.out.println(output);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter Move\n");
+            try {
+                String input = br.readLine();
+                Move move = new Move(input);
+                board.applyMove(move);
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
         }
     }
 
