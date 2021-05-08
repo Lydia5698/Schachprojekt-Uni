c4e5a17bcc67d2d2a278f4bcecf12dffc3b2190a
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2021-05-08 22:42:47 +0200

    Cleard Warnings in Manuals for getAttackers

--- src/main/java/chess/Manuals.java
+++ src/main/java/chess/Manuals.java
@@ -16,36 +16,34 @@ public class Manuals {
     public Manuals() {
     }
 
-    protected boolean checkIfWayIsNotOccupied(CellIndex start, CellIndex end, Cell[][] checkerBoard){
+    protected boolean checkIfWayIsNotOccupied(CellIndex start, CellIndex end, Cell[][] checkerBoard) {
         Cell startCell = checkerBoard[start.getRow()][start.getColumn()];
         Cell endCell = checkerBoard[end.getRow()][end.getColumn()];
         boolean colourStart = startCell.getMinion().isBlack();
-        boolean colourEnd ;
+        boolean colourEnd;
         boolean notOccupied = true;
 
 
         // endfeld nicht leer
-        if(!(endCell.isEmpty())){
+        if (!(endCell.isEmpty())) {
             colourEnd = endCell.getMinion().isBlack();
             // Figur im Endfeld gleiche Farbe, darf keinen Zug ausfuehren
-            if(colourStart == colourEnd){
+            if (colourStart == colourEnd) {
                 notOccupied = false;
-            }
-            else{ // Gegner im Endfeld
+            } else { // Gegner im Endfeld
                 // schreibe den Buschstaben vom geschlagenem Minion in beaten
                 Minion minion = endCell.getMinion();
                 beaten.add(String.valueOf(minion.print_minions()));
             }
         }
         // check if minion = knigth ja- prüfe endfeld nein-prüfe zwischen felder
-        if (String.valueOf(startCell.getMinion().getMinion_type()).equals("N")){
+        if (String.valueOf(startCell.getMinion().getMinion_type()).equals("N")) {
             return notOccupied;
-        }
-        else{
-            int diffRow = start.getRow() - end.getRow() ; //positiv dann gehen wir nach oben, negativ nach unten (weil wir von oben zählen)
+        } else {
+            int diffRow = start.getRow() - end.getRow(); //positiv dann gehen wir nach oben, negativ nach unten (weil wir von oben zählen)
             int diffColumn = start.getColumn() - end.getColumn();//negativ nach rechts, positiv nach links
 
-            for (int i = 1 ; i < (Math.max(Math.abs(diffRow),Math.abs(diffColumn))) ; i++){
+            for (int i = 1; i < (Math.max(Math.abs(diffRow), Math.abs(diffColumn))); i++) {
                 //diagonal
                 if (Math.abs(diffRow) == Math.abs(diffColumn)) {
                     // next celle checken
@@ -59,19 +57,19 @@ public class Manuals {
                     }
                 }
                 // vertical
-                else if (Math.abs(diffRow)>Math.abs(diffColumn)){
-                    int stepR = (diffRow/Math.abs(diffRow))*i;
+                else if (Math.abs(diffRow) > Math.abs(diffColumn)) {
+                    int stepR = (diffRow / Math.abs(diffRow)) * i;
                     Cell nextCell = checkerBoard[start.getRow() - stepR][start.getColumn()];
-                    if(!(nextCell.isEmpty())){
+                    if (!(nextCell.isEmpty())) {
                         notOccupied = false;
                         break;
                     }
                 }
                 //horizontal
-                else if (Math.abs(diffRow)<Math.abs(diffColumn)){
-                    int stepC = (diffColumn/Math.abs(diffColumn))*i;
+                else if (Math.abs(diffRow) < Math.abs(diffColumn)) {
+                    int stepC = (diffColumn / Math.abs(diffColumn)) * i;
                     Cell nextCell = checkerBoard[start.getRow()][start.getColumn() - stepC];
-                    if(!(nextCell.isEmpty())){
+                    if (!(nextCell.isEmpty())) {
                         notOccupied = false;
                         break;
                     }
@@ -81,19 +79,19 @@ public class Manuals {
         return notOccupied;
     }
 
-    public List<String> getBeaten(){
+    public List<String> getBeaten() {
         return beaten;
     }
 
-    public List getAttackers(Boolean colour, Cell[][] checker){
-        List<Cell> attackers = new ArrayList<Cell>();
+    public List<Cell> getAttackers(Boolean colour, Cell[][] checker) {
+        List<Cell> attackers = new ArrayList<>();
         CellIndex kingIndex = coordinatesKing(colour, checker);
         boolean kingColour = checker[kingIndex.getRow()][kingIndex.getColumn()].getMinion().isBlack();
 
-        for(int row=0; row<8; row++){
-            for(int col=0; col<8; col++){
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
                 Cell tmpCell = checker[row][col];
-                if(!(tmpCell.isEmpty())) {/*add is Valid movement of minion on field tempcell to king cell && kingcolour != tempcolour*/
+                if (!(tmpCell.isEmpty())) {/*add is Valid movement of minion on field tempcell to king cell && kingcolour != tempcolour*/
                     if ((checkIfValidMove(new CellIndex(row, col), kingIndex, checker)) && kingColour != tmpCell.getMinion().isBlack()) {
                         attackers.add(tmpCell);
                     }
@@ -103,12 +101,12 @@ public class Manuals {
         return attackers;
     }
 
-    CellIndex coordinatesKing(boolean colour, Cell[][] checkerBoard){
+    CellIndex coordinatesKing(boolean colour, Cell[][] checkerBoard) {
         CellIndex kingCell = new CellIndex(9, 9);
-        for(int row = 0; row < 8; row++){
-            for(int column = 0; column < 8; column++){
-                if(!checkerBoard[row][column].isEmpty()){
-                    if(String.valueOf(checkerBoard[row][column].getMinion().getMinion_type()).equals("K") && checkerBoard[row][column].getMinion().isBlack() == colour){
+        for (int row = 0; row < 8; row++) {
+            for (int column = 0; column < 8; column++) {
+                if (!checkerBoard[row][column].isEmpty()) {
+                    if (String.valueOf(checkerBoard[row][column].getMinion().getMinion_type()).equals("K") && checkerBoard[row][column].getMinion().isBlack() == colour) {
                         kingCell.setRow(row);
                         kingCell.setColumn(column);
                     }
@@ -118,16 +116,15 @@ public class Manuals {
         return kingCell;
     }
 
-    protected boolean checkIfValidMove(CellIndex start, CellIndex end, Cell [][] checkerboard){
+    protected boolean checkIfValidMove(CellIndex start, CellIndex end, Cell[][] checkerboard) {
         Cell startCell = checkerboard[start.getRow()][start.getColumn()];
         Minion minion = startCell.getMinion();
-        if(minion.validMove(start, end)) {
+        if (minion.validMove(start, end)) {
             //unblocked? yes, apply, no dont do it
             return checkIfWayIsNotOccupied(start, end, checkerboard);
         }
         return false;
     }
-
 
 
 }
